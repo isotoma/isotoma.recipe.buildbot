6262ba0d8446db6f5ec1840d53577e3988c54b6f
John Carr <john.carr@isotoma.com>
2010-05-31 23:41:03 +0100

    Fix upgrader script

--- isotoma/recipe/buildbot/upgrader.py
+++ isotoma/recipe/buildbot/upgrader.py
@@ -7,7 +7,7 @@ except ImportError:
 def run(spec, basedir):
     print "Creating or updating %s" % spec
     s = dbspec.DBSpec.from_url(spec)
-    sm = schema.DBSchemaManager(s, basedir)
+    sm = DBSchemaManager(s, basedir)
     print "Currently at %s, upgrading to latest" % sm.get_db_version()
     sm.upgrade()
     print "Done"
