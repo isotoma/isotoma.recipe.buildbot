98729ae74602d48a5195f7f40044430cf80a45a1
Tom Wardill <tom@howrandom.net>
2010-08-09 16:42:18 +0100

    Working recipe for a trac deployment

--- isotoma.recipe.trac/isotoma/recipe/trac/__init__.py
+++ isotoma.recipe.trac/isotoma/recipe/trac/__init__.py
@@ -1,10 +1,62 @@
+import os
+import sys
+
+import pkg_resources
+import zc.buildout
+import zc.recipe.egg
+
+from trac.admin.console import TracAdmin
+
 class Recipe(object):
 
     def __init__(self, buildout, name, options):
-        pass
+        self.buildout, self.name, self.options = buildout, name, options
+
+        options['location'] = os.path.join(
+            buildout['buildout']['parts-directory'],
+            self.name
+            )
+
+        options['bin-directory'] = buildout['buildout']['bin-directory']
+        options['executable'] = sys.executable
 
     def install(self):
-        pass
+        options = self.options
+
+        # create our run scripts
+        entry_points = [('trac-admin', 'trac.admin.console', 'run'),
+                        ('tracd', 'trac.web.standalone', 'main')]
+
+        zc.buildout.easy_install.scripts(
+                entry_points, pkg_resources.working_set,
+                options['executable'], options['bin-directory']
+                )
+    
+        # create the trac instance
+        location = options['location']
+        project_name = options.get('project-name', 'trac-project')
+        project_name = '"%s"' % project_name
+        project_url = options.get('project-url', 'http://example.com')
+        if not options.has_key('db-type') or options['db-type'] == 'sqlite':
+            db = 'sqlite:%s' % os.path.join('db', 'trac.db')
+        elif options['db-type'] == 'postgres':
+            db_options = {  'user': options['db-username'], 
+                            'pass': options['db-password'], 
+                            'host': options.get('db-hostname', 'localhost'), 
+                            'port': options.get('db-port', '5432')
+                         }
+            db = 'postgres://%(user)s:%(pass)s@%(host)s:%(port)s' % db_options
+
+        repos_type = options.get('repos-type', "")
+        repos_path = options.get('repos-path', "")
+
+        if not os.path.exists(location):
+            os.mkdir(location)
+
+        trac = TracAdmin(location)
+    
+        if not trac.env_check():
+            trac.do_initenv('%s %s %s %s' % (project_name, db, repos_type, repos_path))
 
     def update(self):
         pass
