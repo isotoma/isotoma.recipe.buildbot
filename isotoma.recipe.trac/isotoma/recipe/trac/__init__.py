dd5993ffda56359d1fee008546aa9b23c03fce74
Thomas Wardill <tomwardill@Talyn.local>
2010-08-09 21:28:55 +0100

    Now supports adding eggs, and enabling/disabling eggs

--- isotoma.recipe.trac/isotoma/recipe/trac/__init__.py
+++ isotoma.recipe.trac/isotoma/recipe/trac/__init__.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import ConfigParser
+import shutil
 
 import pkg_resources
 import zc.buildout
@@ -19,6 +21,13 @@ class Recipe(object):
 
         options['bin-directory'] = buildout['buildout']['bin-directory']
         options['executable'] = sys.executable
+        
+        # gather the eggs that we need
+        eggs = options.get('eggs', '').strip().split()
+        
+        self.egg = zc.recipe.egg.Scripts(buildout, name, {
+                    "eggs": "\n".join(["Trac", "isotoma.recipe.trac", ]  + eggs),
+                    })
 
     def install(self):
         options = self.options
@@ -42,7 +51,7 @@ class Recipe(object):
         elif options['db-type'] == 'postgres':
             db_options = {  'user': options['db-username'], 
                             'pass': options['db-password'], 
-                            'host': options.get('db-hostname', 'localhost'), 
+                            'host': options.get('db-host', 'localhost'), 
                             'port': options.get('db-port', '5432')
                          }
             db = 'postgres://%(user)s:%(pass)s@%(host)s:%(port)s' % db_options
@@ -57,6 +66,37 @@ class Recipe(object):
     
         if not trac.env_check():
             trac.do_initenv('%s %s %s %s' % (project_name, db, repos_type, repos_path))
+        
+        # install the eggs that we need
+        self.egg.install()
+        
+        # move the generated config out of the way so we can inherit it
+        trac_ini = os.path.join(location, 'conf', 'trac.ini')
+        global_ini = os.path.join(location, 'conf', 'global.ini')
+
+        # move the existing config
+        shutil.move(trac_ini, global_ini)
+
+        parser = ConfigParser.ConfigParser()
+        
+        # Enable the components we want
+        components_to_enable = options.get('components_enabled', "").strip().split()
+        components_to_disable = options.get('components_disabled', "").strip().split()
+        # if we don't already have this section, add it
+        if (components_to_enable or components_to_disable) and 'components' not in parser.sections():
+                parser.add_section('components')
+                
+        # add the compone
+        for component in components_to_enable:
+            parser.set('components', component, 'enabled')
+        for component in components_to_disable:
+            parser.set('components', component, 'disabled')
+    
+        # add the inheritance line
+        parser.add_section('inherit')
+        parser.set('inherit', 'file', global_ini)
+
+        parser.write(open(trac_ini, 'w'))
 
     def update(self):
         pass
