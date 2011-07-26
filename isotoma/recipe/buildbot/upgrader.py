01c1c88b72b927adfd7f4cf3a8a185d44b25ef53
John Carr <john.carr@isotoma.com>
2011-07-26 22:04:53 +0100

    Support latest buildbot

--- isotoma/recipe/buildbot/upgrader.py
+++ isotoma/recipe/buildbot/upgrader.py
@@ -1,16 +1,31 @@
 import os
-from buildbot.db import dbspec
+
 try:
-    from buildbot.db.schema.manager import DBSchemaManager
+    from buildbot.db import dbspec
+    try:
+        from buildbot.db.schema.manager import DBSchemaManager
+    except ImportError:
+        from buildbot.db.schema import DBSchemaManager
+
+    def run(spec, basedir):
+        print "Creating or updating %s" % spec
+        os.chdir(basedir)
+        s = dbspec.DBSpec.from_url(spec)
+        sm = DBSchemaManager(s, basedir)
+        print "Currently at %s, upgrading to latest" % sm.get_db_version()
+        sm.upgrade()
+        print "Done"
+
 except ImportError:
-    from buildbot.db.schema import DBSchemaManager
 
-def run(spec, basedir):
-    print "Creating or updating %s" % spec
-    os.chdir(basedir)
-    s = dbspec.DBSpec.from_url(spec)
-    sm = DBSchemaManager(s, basedir)
-    print "Currently at %s, upgrading to latest" % sm.get_db_version()
-    sm.upgrade()
-    print "Done"
+    from buildbot.scripts.runner import in_reactor
+    from buildbot.db import connector
+    from buildbot.master import BuildMaster
 
+    @in_reactor
+    def run(spec, basedir):
+        print "Creating or updating %s" % spec
+        os.chdir(basedir)
+
+        db = connector.DBConnector(BuildMaster(basedir), spec, basedir=basedir)
+        return db.model.upgrade()
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '0.0.25'
+version = '0.0.26'
 
 setup(
     name = 'isotoma.recipe.buildbot',
